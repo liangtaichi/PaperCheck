8195a8c5d77c9bc50094f5886e6f97ff9d715684
Your Name <a234421294@163.com>
2020-09-24 23:24:44 +0800

    fuck

--- PaperCheckByJava/src/main/java/main.java
+++ PaperCheckByJava/src/main/java/main.java
@@ -1,4 +1,5 @@
 
+import Utils.Reader;
 import cn.hutool.core.io.file.FileReader;
 import cn.hutool.core.io.file.FileWriter;
 import com.qianxinyao.analysis.jieba.keyword.Keyword;
@@ -22,6 +23,9 @@ public class main {
          *根据得到的inputfile数组的不同元素将文件装到不同的FileReader中。
          * 然后从FileReader写入String
          */
+        List reader1 = Reader.readTxtFile(inputfile[0]);
+        List reader2 = Reader.readTxtFile(inputfile[1]);
+        List reader3 = Reader.readTxtFile(inputfile[2]);
         FileReader fileReader1 = new FileReader(inputfile[0]);
         FileReader fileReader2 = new FileReader(inputfile[1]);
         FileWriter fileWriter = new FileWriter(inputfile[2]);
